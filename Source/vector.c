4d10ff793d6a2a429f03dfa5dbd5a9f041cb3104
Uladzislau Daroshka <zeroforazone@icloud.com>
2024-01-29 18:24:29 +0300

    Create some math functions to work with vectors

--- Source/vector.c
+++ Source/vector.c
@@ -1,14 +1,84 @@
 #include <math.h>
 #include "vector.h"
 
+float vec2_length(const vec2_t v)
+{
+    return sqrt(v.x * v.x + v.y * v.y);
+}
+
+vec2_t vec2_add(const vec2_t a, const vec2_t b)
+{
+    return vec2_t{a.x + b.x, a.y + b.y};
+}
+
+vec2_t vec2_sub(const vec2_t a, const vec2_t b)
+{
+    return vec2_t{a.x - b.x, a.y - b.y};
+}
+
+vec2_t vec2_mul(const vec2_t v, const float factor)
+{
+    return vec2_t{v.x * factor, v.y * factor};
+}
+
+vec2_t vec2_div(const vec2_t v, const float factor)
+{
+    return vec2_t{v.x / factor, v.y / factor};
+}
+
+float vec2_dot(const vec2_t a, const vec2_t b)
+{
+    return a.x * b.x + a.y * b.y;
+}
+
+float vec3_length(const vec3_t v)
+{
+    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+vec3_t vec3_add(const vec3_t a, const vec3_t b)
+{
+    return vec3_t{a.x + b.x, a.y + b.y, a.z + b.z};
+}
+
+vec3_t vec3_sub(const vec3_t a, const vec3_t b)
+{
+    return vec3_t{a.x - b.x, a.y - b.y, a.z - b.z};
+}
+
+vec3_t vec3_mul(const vec3_t v, const float factor)
+{
+    return vec3_t{v.x * factor, v.y * factor, v.z * factor};
+}
+
+vec3_t vec3_div(const vec3_t v, const float factor)
+{
+    return vec3_t{v.x / factor, v.y / factor, v.z / factor};
+}
+
+vec3_t vec3_cross(const vec3_t a, const vec3_t b)
+{
+    return vec3_t
+    {
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x
+    };
+}
+
+float vec3_dot(const vec3_t a, const vec3_t b)
+{
+    return a.x * b.x + a.y * b.y + a.z * b.z;;
+}
+
 vec3_t vec3_rotate_x(const vec3_t v, const float angle)
 {
     const vec3_t rotated_vector =
-        {
+    {
         v.x,
         v.y * cos(angle) - v.z * sin(angle),
         v.y * sin(angle) + v.z * cos(angle)
-        };
+    };
 
     return rotated_vector;
 }
@@ -16,11 +86,11 @@ vec3_t vec3_rotate_x(const vec3_t v, const float angle)
 vec3_t vec3_rotate_y(const vec3_t v, const float angle)
 {
     const vec3_t rotated_vector =
-        {
+    {
         v.x * cos(angle) - v.z * sin(angle),
         v.y,
         v.x * sin(angle) + v.z * cos(angle)
-        };
+    };
 
     return rotated_vector;
 }
@@ -28,11 +98,11 @@ vec3_t vec3_rotate_y(const vec3_t v, const float angle)
 vec3_t vec3_rotate_z(const vec3_t v, const float angle)
 {
     const vec3_t rotated_vector =
-        {
+    {
         v.x * cos(angle) - v.y * sin(angle),
         v.x * sin(angle) + v.y * cos(angle),
         v.z
-        };
+    };
 
     return rotated_vector;
 }
--- Source/vector.h
+++ Source/vector.h
@@ -13,6 +13,21 @@ typedef struct
     float z;
 } vec3_t;
 
+float vec2_length(const vec2_t v);
+vec2_t vec2_add(const vec2_t a, const vec2_t b);
+vec2_t vec2_sub(const vec2_t a, const vec2_t b);
+vec2_t vec2_mul(const vec2_t v, const float factor);
+vec2_t vec2_div(const vec2_t v, const float factor);
+float vec2_dot(const vec2_t a, const vec2_t b);
+
+float vec3_length(const vec3_t v);
+vec3_t vec3_add(const vec3_t a, const vec3_t b);
+vec3_t vec3_sub(const vec3_t a, const vec3_t b);
+vec3_t vec3_mul(const vec3_t v, const float factor);
+vec3_t vec3_div(const vec3_t v, const float factor);
+vec3_t vec3_cross(const vec3_t a, const vec3_t b);
+float vec3_dot(const vec3_t a, const vec3_t b);
+
 vec3_t vec3_rotate_x(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
 vec3_t vec3_rotate_z(vec3_t v, float angle);
