2ed1765741a05627da90c6f29dcbfd429747a905
Uladzislau Daroshka <zeroforazone@icloud.com>
2024-03-12 18:53:51 +0300

    Create FPS camera

--- Source/main.c
+++ Source/main.c
@@ -36,6 +36,7 @@ int num_triangles_to_render = 0;
 
 bool is_running = false;
 int previous_frame_time = 0;
+float delta_time = 0;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Declaration of our global transformation matrices
@@ -108,7 +109,21 @@ void process_input(void)
             if (event.key.keysym.sym == SDLK_5) render_method = RENDER_TEXTURED;
             if (event.key.keysym.sym == SDLK_6) render_method = RENDER_TEXTURED_WIRE;
             if (event.key.keysym.sym == SDLK_c) cull_method = CULL_BACKFACE;
-            if (event.key.keysym.sym == SDLK_d) cull_method = CULL_NONE;
+            if (event.key.keysym.sym == SDLK_x) cull_method = CULL_NONE;
+            if (event.key.keysym.sym == SDLK_UP) camera.position.y += 3.0f * delta_time;
+            if (event.key.keysym.sym == SDLK_DOWN) camera.position.y -= 3.0f * delta_time;
+            if (event.key.keysym.sym == SDLK_a) camera.yaw -= 0.6f * delta_time;
+            if (event.key.keysym.sym == SDLK_d) camera.yaw += 0.6f * delta_time;
+            if (event.key.keysym.sym == SDLK_w)
+            {
+                camera.forward_velocity = vec3_mul(camera.direction, 5.0f * delta_time);
+                camera.position = vec3_add(camera.position, camera.forward_velocity);
+            }
+            if (event.key.keysym.sym == SDLK_s)
+            {
+                camera.forward_velocity = vec3_mul(camera.direction, 5.0f * delta_time);
+                camera.position = vec3_sub(camera.position, camera.forward_velocity);
+            }
             break;
         default:
             break;
@@ -129,24 +144,30 @@ void update(void)
         SDL_Delay(time_to_wait);
     }
 
+    // Get a delta time factor converted to seconds to be used to update our game objects
+    delta_time = (SDL_GetTicks() - previous_frame_time) / 1000.0f;
+
     previous_frame_time = SDL_GetTicks();
 
     // Initialize the counter of triangles to render for the current frame
     num_triangles_to_render = 0;
 
     // Change the mesh scale/rotation/translation values per animation frame
-    // mesh.rotation.x += 0.025f;
-    // mesh.rotation.y += 0.025f;
-    // mesh.rotation.z += 0.025f;
+    // mesh.rotation.x += 1.0f * delta_time;
+    // mesh.rotation.y += 1.0f * delta_time;
+    // mesh.rotation.z += 1.0f * delta_time;
     mesh.translation.z = 5.0f;
 
-    // Change the camera position per animation frame
-    camera.position.x += 0.1f;
-    camera.position.y += 0.1f;
+    // Initialize the target looking at the positive z-axis
+    vec3_t target = {0, 0, 1};
+    mat4_t camera_yaw_rotation = mat4_make_rotation_y(camera.yaw);
+    camera.direction = vec3_from_vec4(mat4_mul_vec4(camera_yaw_rotation, vec4_from_vec3(target)));
 
-    // Create the view matrix looking at hardcoded target point
-    vec3_t target = {0, 0, 5.0f};
+    // Offset the camera position in the direction where the camera is pointing at
+    target = vec3_add(camera.position, camera.direction);
     vec3_t up_direction = {0, 1, 0};
+
+    // Create the view matrix
     view_matrix = mat4_look_at(camera.position, target, up_direction);
 
     // Create a scale, translation and rotation matrices that will be used to multiply the mesh vertices
